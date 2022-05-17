3bff9c99b0fd714e75d15061f36283a9b46e57b0
yuelin.ye <yuelin.ye@xbongbong.com>
2022-05-17 14:09:11 +0800

    feat: init refactor

--- refactor/index.js
+++ refactor/index.js
@@ -1,4 +1,30 @@
-function statement(invoice, plays) {
+export const invoice = {
+    customer: "BigCo",
+    performances: [
+        {
+            "playID": "hamlet",
+            "audience": 55
+        },
+        {
+            "playID": "as-like",
+            "audience": 35
+        },
+        {
+            "playID": "othello",
+            "audience": 40
+        }
+    ]
+}
+
+export const plays = {
+    "hamlet": {"name": "Hamlet", "type": "tragedy"},
+    "as-like": {"name": "As You Like It", "type": "comedy"},
+    "othello": {"name": "Othello", "type": "tragedy"}
+}
+// 剧团则根据观众（audience）人数及剧目类型来向客户收费。
+// 该团目前出演两种戏剧：悲剧（tragedy）和喜剧（comedy）。
+// 给客户发出账单时，剧团还会根据到场观众的数量给出“观众量积分”（volumeCredit）优惠，下次客户再请剧团表演时可以使用积分获得折扣
+export function statement(invoice, plays) {
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `Statement for ${invoice.customer}\n`;
@@ -39,4 +65,115 @@ function statement(invoice, plays) {
     result += `You earned ${volumeCredits} credits\n`;
     return result;
 
+}
+// console.log( statement(invoice, plays))
+
+// 需求修改：
+// 首先，PM希望以HTML格式输出详单
+// 然后添加演出戏剧类型，对戏剧场次的计费方式、积分的计算方式都有影响
+// 需求的变化使重构变得必要，如果一段代码能正常工作，并且不会再被修改，那么完全可以不去重构它。能改进它当然很好
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export function add(...args) {
+  return args.reduce((a, b) => a + b, 0)
+}
+
+
+export function sampleProvinceData() {
+    return {
+        name: "Asia",
+        producers: [
+            {name: "Byzantium", cost: 10, production: 9},
+            {name: "Attalia", cost: 12, production: 10},
+            {name: "Sinope", cost: 10, production: 6},
+        ],
+        demand: 30,
+        price: 20
+    };
+}
+// 一旦业务逻辑的部分开始变复杂，应该把它与UI分离开，以便能更好地理解和测试它。
+export class Province {
+    constructor(doc) {
+        this._name = doc.name;
+        this._producers = [];
+        this._totalProduction = 0;
+        this._demand = doc.demand;
+        this._price = doc.price;
+        doc.producers.forEach(d => this.addProducer(new Producer(this, d)));
+    }
+    addProducer(arg) {
+        this._producers.push(arg);
+        this._totalProduction += arg.production;
+    }
+    get name() {return this._name;}
+    get producers() {return this._producers.slice();}
+    get totalProduction() {return this._totalProduction;}
+    set totalProduction(arg) {this._totalProduction = arg;}
+    get demand() {return this._demand;}
+    set demand(arg) {this._demand = parseInt(arg);}
+    get price() {return this._price;}
+    set price(arg) {this._price = parseInt(arg);}
+    get shortfall() {
+      return this._demand - this.totalProduction;
+    }
+    get profit() {
+        return this.demandValue - this.demandCost;
+    }
+    get demandCost() {
+        let remainingDemand = this.demand;
+        let result = 0;
+        this.producers
+        .sort((a,b) => a.cost - b.cost)
+        .forEach(p => {
+            const contribution = Math.min(remainingDemand, p.production);
+            remainingDemand -= contribution;
+            result += contribution * p.cost;
+        });
+        return result;
+    }
+    get demandValue() {
+        return this.satisfiedDemand * this.price;
+    }
+    get satisfiedDemand() {
+        return Math.min(this._demand, this.totalProduction);
+    }
+
+}
+
+export class Producer {
+    constructor(aProvince, data) {
+        this._province = aProvince;
+        this._cost = data.cost;
+        this._name = data.name;
+        this._production = data.production || 0;
+    }
+    get name() {return this._name;}
+    get cost() {return this._cost;}
+    set cost(arg) {this._cost = parseInt(arg);}
+    get production() {return this._production;}
+    set production(amountStr) {
+        const amount = parseInt(amountStr);
+        const newProduction = Number.isNaN(amount) ? 0 : amount;
+        this._province.totalProduction += newProduction - this._production;
+        this._production = newProduction;
+    }
 }
---
+++ refactor/index.test.js
@@ -0,0 +1,71 @@
+import { describe, it, expect, beforeEach, assert } from 'vitest'
+import { add, sampleProvinceData, Province, statement, invoice, plays } from './index'
+// describe('suite', () => {
+//   it('add', () => {
+//       expect(add()).toBe(0)
+//       expect(add(1)).toBe(1)
+//       expect(add(1, 2, 3)).toBe(6)
+//       assert.equal(add(1, 2, 3), 6);
+//   })
+// })
+describe('province', function() { // 只要测试失败时足以识别出对应的测试
+  let asia
+  beforeEach(function() {
+   asia = new Province(sampleProvinceData());
+  });
+  it('shortfall', function() {
+    // const asia = new Province(sampleProvinceData()); // 设置好是测试所需要的数据和对象等
+    // 验证数据和对象等是否正确
+    expect(asia.shortfall).equal(5);
+  });
+  it('profit', function() {
+    // const asia = new Province(sampleProvinceData()); //测试随即产生了一些重复代码,它们都在第一行里初始化了同一个数据和对象
+    expect(asia.profit).equal(230);
+  });
+  // 验证数据修改
+  it('change production', function() {
+    asia.producers[0].production = 20;
+    expect(asia.shortfall).equal(-6);
+    expect(asia.profit).equal(292);
+  });
+  // 边界情况
+  it('zero demand', function() {
+    asia.demand = 0;
+    expect(asia.shortfall).equal(-25);
+    expect(asia.profit).equal(0);
+  });
+  it('negative demand', function() {
+    asia.demand = -1;
+    expect(asia.shortfall).equal(-26);
+    expect(asia.profit).equal(-10);
+  });
+  it('empty string demand', function() {
+    asia.demand = "";
+    expect(asia.shortfall).NaN;
+    expect(asia.profit).NaN;
+  });
+});
+// 上面的测试都聚焦于正常的行为上，被称为“正常路径”（happy path）
+// 探测边界条件
+describe('no producers', function() {
+  let noProducers;
+  beforeEach(() => {
+    const data = {
+      name: "No proudcers",
+      producers: [],
+      demand: 30,
+      price: 20
+    };
+    noProducers = new Province(data);
+  });
+  it('shortfall', function() {
+    expect(noProducers.shortfall).equal(30);
+  });
+  it('profit', function() {
+    expect(noProducers.profit).equal(0);
+  });
+})
+
+// 不要因为测试无法捕捉所有的bug就不写测试，因为测试的确可以捕捉到大多数bug
+// 与编程的许多方面类似，测试也是一种迭代式的活动。除非你技能非常纯熟，或者非常幸运，否则你很难第一次就把测试写对。
+
