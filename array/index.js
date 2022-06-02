fa467291f7e34494c736615bf380745d06ed273f
yuelin.ye <yuelin.ye@xbongbong.com>
2022-06-02 09:06:21 +0800

    add: some practice

--- array/index.js
+++ array/index.js
@@ -1,62 +1,89 @@
+
 // filter
 export function filterArray(arr, callback){
-  const result = []
-  for (const elem of arr) {
+  // for-of
+  // const result = []
+  // for (const elem of arr) {
+  //   if(callback(elem)){
+  //     result.push(elem)
+  //   }
+  // }
+  // return result
+  // reduce
+  return arr.reduce((result, elem) => {
     if(callback(elem)){
       result.push(elem)
+    }
+    return result
+  },[])
+
+  // flatmap
+}
+// map
+export function mapArray(arr, callback) {
+  // for-of
+  // const result = []
+  // for (const elem of arr) {
+  //   result.push(callback(elem))
+  // }
+  // return result
+  // reduce
+  return arr.reduce((result, elem) => {
+    result.push(callback(elem))
+    return result
+  },[])
+}
+// expanding
+export function collectFruits(persons) {
+  // for-of
+  // const result = []
+  // for (const person of persons) {
+  //   result.push(...person.fruits)
+  // }
+  // return result
+  // reduce
+  return persons.reduce((result, person) => {
+    result.push(...person.fruits)
+    return result
+  },[])
+}
+// filter-mapping
+export function getTitles(movies, minRating) {
+  // for-of
+  const result = []
+  for (const movie of movies) {
+    if(movie.rating >= minRating){
+      result.push(movie.title)
     }
   }
   return result
 }
-// map
-export function mapArray(arr, callback) {
-  const result = [];
-  for (const elem of arr) {
-    result.push(callback(elem));
-  }
-  return result;
-}
-// expanding
-export function collectFruits(persons) {
-  const result = [];
-  for (const person of persons) {
-    result.push(...person.fruits);
-  }
-  return result;
-}
-// filter-mapping
-export function getTitles(movies, minRating) {
-  const result = [];
-  for (const movie of movies) {
-    if (movie.rating >= minRating) { // (A)
-      result.push(movie.title); // (B)
-    }
-  }
-  return result;
-}
 // Computing a summary
 export function getAverageGrade(students) {
-  let sumOfGrades = 0;
+  // for-of
+  let result = 0
   for (const student of students) {
-    sumOfGrades += student.grade;
+    result += student.grade
   }
-  return sumOfGrades / students.length;
+  return result / students.length
 }
 // Finding
 export function findInArray(arr, callback) {
+  // for-of
   for (const [index, value] of arr.entries()) {
-    if (callback(value)) {
-      return { index, value }; // (A)
+    if (callback(value)){
+      return { index, value }
     }
   }
-  return undefined;
+  return undefined
 }
 // Checking a condition 
 export function everyArrayElement(arr, condition) {
+  // for-of
   for (const elem of arr) {
-    if (!condition(elem)) {
-      return false; // (A)
+    if(!condition(elem)){
+      return false
     }
   }
-  return true;
+  return true
 }
